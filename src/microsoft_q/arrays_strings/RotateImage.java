9edfa1e866e877d2ac96d1cef3fb23310c35be1e
Siwei Xing <luxness.int@gmail.com>
2023-01-14 10:46:20 -0800

    20230114 Alpha Solution of two Array related problems

---
+++ src/microsoft_q/arrays_strings/RotateImage.java
@@ -0,0 +1,13 @@
+package microsoft_q.arrays_strings;
+
+public class RotateImage {
+    public static void main(String[] args) {
+
+    }
+
+    static class Solution{
+        public void rotate(int[][] matrix) {
+
+        }
+    }
+}
---
+++ src/microsoft_q/arrays_strings/SetMatrixZeroes.java
@@ -0,0 +1,26 @@
+package microsoft_q.arrays_strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SetMatrixZeroes {
+    public static void main(String[] args) {
+
+    }
+
+    static class Solution {
+        public void setZeroes(int[][] matrix) {
+            //traverse the whole matrix and record coordinate of '0' cell
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    if (matrix[i][j] == 0) {
+                        list.add();
+                    }
+                }
+            }
+            //modify the matrix based on record
+        }
+    }
+
+}
