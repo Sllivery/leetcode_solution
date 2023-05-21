58dbf395481d7e1ea293de7095935f82ed3cf8e8
Siwei Xing <luxness.int@gmail.com>
2023-05-20 22:06:02 -0700

    finally!

--- src/microsoft_q/arrays_strings/RotateImage.java
+++ src/microsoft_q/arrays_strings/RotateImage.java
@@ -7,13 +7,14 @@ public class RotateImage {
         solution.rotate(m);
         for (int i = 0; i < m.length; i++) {
             for (int j = 0; j < m[0].length; j++) {
-                System.out.println(m[i][j]);
+                System.out.print(m[i][j]);
             }
+            System.out.println("");
         }
     }
 
     static class Solution{
-        public void rotate(int[][] matrix) {
+        public void myRotate(int[][] matrix) {
             int l = 0, t = 0;
             int r = matrix.length - 1, b = matrix[0].length;
             while ( l < r ){
@@ -33,6 +34,24 @@ public class RotateImage {
                 }
                 l += 1;
                 r -= 1;
+            }
+        }
+
+        public void rotate(int[][] matrix) {
+            //outter loop controls the martrix layers
+            //inner loop controls the round
+            //I'm the BEEEEEEEEEEEEST!!!!!!!!!!!!!!!!!!!!!!!
+            int times = 0;
+            for (int i = 0; i <= matrix.length / 2; i++) {
+                int currentLength = matrix.length - 2 * times;
+                for (int k = 0, j = i; j < i + currentLength - 1; j++, k++) {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[i + currentLength - 1 - k][i];
+                    matrix[i + currentLength - 1 - k][i] = matrix[i + currentLength - 1][i + currentLength - 1 - k];
+                    matrix[i + currentLength - 1][i + currentLength - 1 - k] = matrix[j][i + currentLength - 1];
+                    matrix[j][i + currentLength - 1] = temp;
+                }
+                times++;
             }
         }
     }
